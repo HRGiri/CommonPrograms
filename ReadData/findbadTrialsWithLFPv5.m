7af32bfff647e23e1a2e24fc7fef351f3163f964
Aniket Mandal <aniket2014.mandal@gmail.com>
2026-02-16 14:36:24 +0530

    Made corrections to errors; changed function argument calling type from 'Positional' based to 'Name-Value' type

--- ReadData/findbadTrialsWithLFPv5.m
+++ ReadData/findbadTrialsWithLFPv5.m
@@ -11,9 +11,7 @@
 % 2. Bad trial percentage on that electrode
 % 3. Average PSD slope in a certain range 
 
-function [allBadTrials,badTrials] = findbadTrialsWithLFPv5(monkeyName,expDate,protocolName,folderSourceString,gridType,processAllElectrodes,...
-    checkTheseElectrodes,highPassCutOff,checkPeriod,timeThreshold,maxLimit,minLimit,rmsThreshold,checkPsdPeriod,psdThreshold,checkPsdSlopePeriod,...
-    badTrialPercentageThreshold,showElectrodes,marginalsFlag,saveDataFlag,badTrialName)
+function [allBadTrials,badTrials] = findbadTrialsWithLFPv5(monkeyName,expDate,protocolName,folderSourceString, opts)
 
 % Defining named arguments here so that optional arguments can be provided
 % using the name of the argument
@@ -22,24 +20,42 @@ arguments
     expDate                                     % Required
     protocolName                                % Required
     folderSourceString                          % Required
-    gridType char = 'Microelectrode';       
-    processAllElectrodes logical = 0;
-    checkTheseElectrodes double = 49:96;        % V1 for Jojo
-    highPassCutOff double = [];                 % No high pass filtering done when empty
-    checkPeriod double = [-0.7 -0.1; 0.4 1.2];  % nx2 arrays where n=# check periods
-    timeThreshold double = 6;
-    maxLimit double = 1000; 
-    minLimit double = -1000;
-    rmsThreshold double = [1.5 35];             % [lower upper]
-    checkPsdPeriod double = [-0.7 -0.2; 0.7 1.2];  % nx2 arrays where n=# check periods
-    psdThreshold double = 6;
-    checkPsdSlopePeriod double = [-0.7 -0.2];
-    badTrialPercentageThreshold = 40;
-    showElectrodes double = [];                 % electrodes to plot
-    marginalsFlag logical = 0;
-    saveDataFlag logical = 0;    
-    badTrialName char = '_v5';                  % string to be added to the bad trial file name
+    opts.gridType char = 'Microelectrode';       
+    opts.processAllElectrodes logical = 0;
+    opts.checkTheseElectrodes double = 49:96;        % V1 for Jojo
+    opts.highPassCutOff double = [];                 % No high pass filtering done when empty
+    opts.checkPeriod double = [-0.7 -0.1; 0.4 1.2];  % nx2 arrays where n=# check periods
+    opts.timeThreshold double = 6;
+    opts.maxLimit double = 1000; 
+    opts.minLimit double = -1000;
+    opts.rmsThreshold double = [1.5 35];             % [lower upper]
+    opts.checkPsdPeriod double = [-0.7 -0.2; 0.7 1.2];  % nx2 arrays where n=# check periods
+    opts.psdThreshold double = 6;
+    opts.checkPsdSlopePeriod double = [-0.7 -0.2];
+    opts.badTrialPercentageThreshold = 40;
+    opts.showElectrodes double = [];                 % electrodes to plot
+    opts.marginalsFlag logical = 0;
+    opts.saveDataFlag logical = 0;    
+    opts.badTrialName char = '_v5';                  % string to be added to the bad trial file name
 end
+
+gridType = opts.gridType;
+processAllElectrodes = opts.processAllElectrodes;
+checkTheseElectrodes = opts.checkTheseElectrodes;
+highPassCutOff = opts.highPassCutOff;
+checkPeriod = opts.checkPeriod;
+timeThreshold = opts.timeThreshold;
+maxLimit = opts.maxLimit;
+minLimit = opts.minLimit;
+rmsThreshold = opts.rmsThreshold;
+checkPsdPeriod = opts.checkPsdPeriod;
+psdThreshold = opts.psdThreshold;
+checkPsdSlopePeriod = opts.checkPsdSlopePeriod;
+badTrialPercentageThreshold = opts.badTrialPercentageThreshold;
+showElectrodes = opts.showElectrodes;
+marginalsFlag = opts.marginalsFlag;
+saveDataFlag = opts.saveDataFlag;
+badTrialName = opts.badTrialName;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Initializations %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 impedanceCutOff = 25; % KOhm
@@ -48,11 +64,10 @@ tapersPSD = 1; % No. of tapers used for computation of slopes
 slopeRange = {[56 86]}; % Hz, slope range used to compute slopes
 freqsToAvoid = {[0 0] [8 12] [46 54] [96 104]}; % Hz
 
-end
 
 % % Aniket % %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Get data %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-folderName = fullfile(folderSourceString,'data',subjectName,gridType,expDate,protocolName);
+folderName = fullfile(folderSourceString,'data',monkeyName,gridType,expDate,protocolName);
 folderSegment = fullfile(folderName,'segmentedData');
 lfpInfo = load(fullfile(folderSegment,'LFP','lfpInfo.mat'));
 
@@ -61,12 +76,12 @@ timeVals = lfpInfo.timeVals;
 % for EEG, numElectrodes and loading data corr. to all the electrodes would
 % be different
 if processAllElectrodes % compute bad trials for all the saved electrodes
-    numElectrodes = length(electrodesStored);
+    numElectrodes = length(lfpInfo.electrodesStored);
 else % compute bad trials for only the electrodes mentioned
     numElectrodes = length(checkTheseElectrodes);
 end
 
-x = load(fullfile(folderSegment,'LFP',['elec' num2str(electrodesStored(1)) '.mat'])); r = size(x.analogData, 1); c = size(x.analogData, 2); % get size of LFPdata for 1 electrode
+x = load(fullfile(folderSegment,'LFP',['elec' num2str(lfpInfo.electrodesStored(1)) '.mat'])); r = size(x.analogData, 1); c = size(x.analogData, 2); % get size of LFPdata for 1 electrode
 lfpData = zeros(numElectrodes, r, c); % initializing array to store LFP data corr. to all the electrodes
 nameElec = cell(1,numElectrodes);
 
@@ -74,15 +89,15 @@ hW1 = waitbar(0,'collecting data...');
 for i=1:numElectrodes
     if processAllElectrodes
         iElec = electrodesStored(i);
-        waitbar((i-1)/numElectrodes,hW1,['collecting data from electrode: ' num2str(iElec) ' of ' num2str(numElectrodes)]);
+        waitbar((i-1)/numElectrodes,hW1,['collecting data from electrode: ' num2str(i) ' of ' num2str(numElectrodes)] );
         
         clear x; x = load(fullfile(folderSegment,'LFP',['elec' num2str(iElec) '.mat'])); % Load LFP Data
         lfpData(i,:,:) = x.analogData; %#ok<AGROW>
-        nameElec{i} = ['elec' num2str(iElec)];
+        nameElec{i} = ['elec' num2str(iElec)]; 
         % disp(nameElec{i});
     else
         iElec = checkTheseElectrodes(i);
-        waitbar((i-1)/numElectrodes,hW1,['collecting data from electrode: ' num2str(iElec) ' of ' num2str(numElectrodes)]);
+        waitbar((i-1)/numElectrodes,hW1,['collecting data from electrode: ' num2str(i) ' of ' num2str(numElectrodes)]);
         
         clear x; x = load(fullfile(folderSegment,'LFP',['elec' num2str(iElec) '.mat'])); % Load LFP Data
         lfpData(i,:,:) = x.analogData; %#ok<AGROW>
@@ -112,11 +127,11 @@ params.fpass    = [0 200];
 params.trialave = 0;
 
 %%%%%%%%%%%%%%%%%%%%% Applying Filter (if instructed) %%%%%%%%%%%%%%%%%%%%%
-if exist('highPassCutOff','var') || ~isempty(highPassCutOff)    % high pass filter    % can do this after the impedance check as well
+if ~isempty(highPassCutOff)    % high pass filter    % can do this after the impedance check as well
     for i=1:numElectrodes
-        fprintf('Applying a 4th order Butterworth High pass filter with cutoff=%d Hz', highPassCutOff)
-        [lfpData(i, :, :), filterStr] = applyFilter(lfpData(iElec, :, :),2000,'butter','high',4,highPassCutOff);
+        [lfpData(i, :, :), filterStr] = applyFilter(squeeze(lfpData(i, :, :)),2000,'butter','high',4,highPassCutOff);
     end
+    fprintf('Applied a 4th order Butterworth High pass filter with cutoff=%d Hz\n', highPassCutOff)
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%% Bad Trial Analysis %%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -138,6 +153,7 @@ allBadTrials = cell(1,numElectrodes);
 hW1 = waitbar(0,'Processing electrodes...');
 
 for iElec=1:numElectrodes
+
     waitbar((iElec-1)/numElectrodes,hW1,['Processing electrode: ' num2str(iElec) ' of ' num2str(numElectrodes)]);
     % if ~GoodElec_Z(iElec); allBadTrials{iElec} = NaN; continue; end % Analyzing only those electrodes with impedance < 25k
     clear analogData; analogData = squeeze(lfpData(iElec,:,:));
@@ -150,6 +166,7 @@ for iElec=1:numElectrodes
     numCheckPeriods = size(checkPeriod, 1);    
     for j=1:numCheckPeriods
     % determine indices corresponding to the check period
+
         checkPeriodIndices = timeVals>=checkPeriod(j,1) & timeVals<=checkPeriod(j,2);
         analogDataSegment = analogData(:, checkPeriodIndices);
         
@@ -162,22 +179,22 @@ for iElec=1:numElectrodes
         maxData(badTrials1) = meanData(badTrials1); % exclude trial indices already in badTrials1
         minData  = min(analogDataSegment,[],2)';
         minData(badTrials1) = meanData(badTrials1); % exclude trial indices already in badTrials1
-
+   
         clear tmpNoisyTrials tmpBadMinValTrials tmpBadMaxValTrials
         tmpNoisyTrials = unique([find(maxData > meanData + timeThreshold * stdData) find(minData < meanData - timeThreshold * stdData)]);
         tmpBadMinValTrials = unique(find(maxData > maxLimit));
         tmpBadMaxValTrials = unique(find(minData < minLimit));
+        
 
         % consolidate across checkPeriods
-        noisyTrials = unique([noisyTrials tmpNoisyTrials]);
-        badMaxValTrials = unique([badMaxValTrials, tmpBadMaxValTrials]);
-        badMinValTrials = unique([badMinValTrials tmpBadMinValTrials]);
-
+        noisyTrials = unique([noisyTrials(:); tmpNoisyTrials(:)]);   % save the consolidated arrays as column vectors
+        badMaxValTrials = unique([badMaxValTrials(:); tmpBadMaxValTrials(:)]);
+        badMinValTrials = unique([badMinValTrials(:); tmpBadMinValTrials(:)]);
 
         % 2.1.2 RMS Check
         % calculate RMS Values for each trial
         if ~isempty(analogDataSegment)
-            allTrialsRMS = rms(analogDataSegment, 2);
+            allTrialsRMS = sqrt(mean(analogDataSegment.^2, 2));
             allTrialsRMS(badTrials1) = mean(rmsThreshold);        % exclude trial indices already in badTrials
         else
             allTrialsRMS=[];
@@ -192,9 +209,9 @@ for iElec=1:numElectrodes
 
         % add list of bad RMS trials in this checkPeriod to list containing all bad RMS trials for all the checkPeriods
         badRmsTrials = unique([badRmsTrials tmpBadRmsTrials]);
-
+      
         % consolidate all bad trials till now (noisy, badMinVal, badMaxVal, badRms) 
-        badTrials1 = unique([noisyTrials badMaxValTrials badMinValTrials badRmsTrials]);  % badTrials1 --> all bad trials that fail Trial Thresholding
+        badTrials1 = unique([noisyTrials(:); badMaxValTrials(:); badMinValTrials(:); badRmsTrials(:)]);  % badTrials1 --> all bad trials that fail Trial Thresholding
 
         % removing all bad Trials till now
         if ~isempty(analogDataSegment)
@@ -213,14 +230,25 @@ for iElec=1:numElectrodes
         tBoolTrials = sum((analogDataSegment > ones(numTrials,1)*tDplus) | (analogDataSegment < ones(numTrials,1)*tDminus),2);
         % didn't do exclusion of trials which failed timeThresholding check in previous checkPeriods here, hopefully no hit on performance
         
-        clear badTrialsTimeThres
+        clear tmpBadTrialsTimeThres
         tmpBadTrialsTimeThres = find(tBoolTrials>0);   
-
+    
         % consolidate across checkPeriods
-        badTrialsTimeThres = unique([badTrialsTimeThres tmpBadTrialsTimeThres]);  % badTrialstimeThres --> all bad trials that further fail Time Thresholding
-
+        badTrialsTimeThres = unique([badTrialsTimeThres(:); tmpBadTrialsTimeThres(:)]);  % badTrialstimeThres --> all bad trials that further fail Time Thresholding
+        
      % 2.3 Frequency Thresholding
 
 
     end
+
+    % potential allBadTrials storing template
+    allBadTrials{iElec}.noisyTr = noisyTrials;
+    allBadTrials{iElec}.badMaxValTr = badMaxValTrials;
+    allBadTrials{iElec}.badMinValTr = badMinValTrials;
+    allBadTrials{iElec}.badTimeThres = badTrialsTimeThres;
 end
+
+close(hW1);
+
+% decide what badTrials does
+badTrials = 0;    % placeholder value, might show some error if not declared
